872f08e499a8095e537325fb9c3a35e00274adb0
taigi100 <contact@taigi100.com>
2023-12-27 21:10:56 +0200

    Day 2 - good at adding features, not great at keeping it short :)

---
+++ tccday2.lua
@@ -0,0 +1,49 @@
+function TIC()
+    R=math.random
+    t = time() // 100
+    x,y,p = mouse()
+    cls(8)
+    for c=1,100 do 
+        pix(c*15%240,(c+t)%110,12)
+    end 
+    rect(0,106,240,30,12)rect(120,96,10,10,3)
+    for c=0,15,5 do tri(c+85,96-c*2,125,56-2*c,165-c,96-2*c,6)end 
+    rect(124,23,2,5,4)circb(124,24,10,4)
+    for c=1,4 do for h=1-c,c-1 do circ(h*10+124,c*15+26,3,1+t%3)end end
+    if p == true then
+        circb(x,y,ct,12)
+        ct = ct + 1;
+    else
+        ct = 0
+    end
+end
+
+-- <TILES>
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <TRACKS>
+-- 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+-- </TRACKS>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
+
